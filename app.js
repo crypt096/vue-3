0712e3f56f7d61f8e07520709f66d959842269db
crypt0 <acvjetan@gmail.com>
2020-12-16 20:06:31 +0100

    Add dynamic classes.

--- app.js
+++ app.js
@@ -5,17 +5,20 @@ const app = Vue.createApp({
       books: [{
           title: 'name of the wind',
           author: 'patrick rothfuss',
-          img: './assets/1.jpg'
+          img: './assets/1.jpg',
+          isFav: true
         },
         {
           title: 'the way of kings',
           author: 'brandon sanderson',
-          img: './assets/2.jpg'
+          img: './assets/2.jpg',
+          isFav: false
         },
         {
           title: 'the final empire',
           author: 'brandon sanderson',
-          img: './assets/3.jpg'
+          img: './assets/3.jpg',
+          isFav: true
         }
       ],
       showBooks: true,
