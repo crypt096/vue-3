3ff018344dcc9cc700593088bce8e2ff5699cc5a
crypt0 <acvjetan@gmail.com>
2020-12-16 19:45:18 +0100

    Add other mouse events.

--- app.js
+++ app.js
@@ -5,11 +5,23 @@ const app = Vue.createApp({
       author: 'Brandon Sanderson',
       age: 45,
       showBooks: true,
+      x: 0,
+      y: 0,
     }
   },
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
+    },
+    handleEvent(e, data) {
+      console.log(e, e.type)
+      if (data) {
+        console.log(data)
+      }
+    },
+    handleMouseMove(e) {
+      this.x = e.offsetX
+      this.y = e.offsetY
     }
   }
 })
