ecb7b79dc701abdbce6c9238c386e6d17d34dfe1
crypt0 <acvjetan@gmail.com>
2020-12-16 20:19:50 +0100

    Fav book challenge solution.

--- app.js
+++ app.js
@@ -28,7 +28,14 @@ const app = Vue.createApp({
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
     },
+    toggleIsFav(book) {
+      book.isFav = !book.isFav
+    }
   }
 })
 
 app.mount('#app')
+
+// Challenge - Add to Favs
+//  - attach a click event to each li tag ( for each book )
+//  - when a user clicks an li, toggle the 'isFav' property of that book
