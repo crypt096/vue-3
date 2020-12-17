23e7ae257a66d7d27ac79fb712cc6a04823272a6
crypt0 <acvjetan@gmail.com>
2020-12-17 16:09:58 +0100

    Add scoped and global css.

--- modal-project/src/components/Modal.vue
+++ modal-project/src/components/Modal.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="backdrop">
     <div class="modal">
+      <h1>Modal title</h1>
       <p>Modal content</p>
     </div>
   </div>
@@ -18,11 +19,22 @@ export default {};
   background: white;
   border-radius: 10px;
 }
+
 .backdrop {
   top: 0;
   position: fixed;
   background: rgba(0, 0, 0, 0.5);
   width: 100%;
   height: 100%;
+}
+
+.modal h1 {
+  color: #03cfb4;
+  border: none;
+  padding: 0;
+}
+
+.modal p {
+  font-style: normal;
 }
 </style>
